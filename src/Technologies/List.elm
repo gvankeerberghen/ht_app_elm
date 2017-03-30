e0a0aac54ac00b20630d6be5a5f256088ecfa963
Floran Hachez <floran.hachez@gmail.com>
2017-03-30 22:07:18 +0200

    Add material design to Technology page

--- src/Technologies/List.elm
+++ src/Technologies/List.elm
@@ -18,8 +18,16 @@ view response =
 
 nav : Html Msg
 nav =
-    div [ class "clearfix mb2 white bg-black" ]
-        [ div [ class "left p2" ] [ text "Technologies" ] ]
+    div [ class "mdl-layout mdl-js-layout mdl-layout--fixed-header" ]
+        [ 
+            header [ class "mdl-layout__header"] 
+            [  
+                div [ class "mdl-layout__header-row" ] 
+                [
+                     span [ class "mdl-layout-title"] [ text "Technologies"] 
+                ] 
+            ]
+         ]
 
 
 maybeList : WebData (List Technology) -> Html Msg
@@ -40,17 +48,21 @@ maybeList response =
 
 list : List Technology -> Html Msg
 list technologies =
-    div [ class "p2" ]
-        [ table []
-            [ thead []
-                [ tr []
-                    [ th [] [ text "Id" ]
-                    , th [] [ text "Name" ]
-                    , th [] [ text "Votes" ]
+    div [ class "mdl-grid" ]
+        [
+            div [class "mdl-cell mdl-cell--12-col"]
+                [ table [ class "mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp"]
+                    [ thead []
+                        [ tr []
+                            [ th [] [ text "Id" ]
+                            , th [ class "mdl-data-table__cell--non-numeric" ] [ text "Name" ]
+                            , th [] [ text "Votes" ]
+                            , th [] [ text "Edit" ]
+                            ]
+                        ]
+                    , tbody [] (List.map techRow technologies)
                     ]
                 ]
-            , tbody [] (List.map techRow technologies)
-            ]
         ]
 
 
@@ -58,7 +70,7 @@ techRow : Technology -> Html Msg
 techRow tech =
     tr []
         [ td [] [ text (toString tech.id) ]
-        , td [] [ text tech.name ]
+        , td [ class "mdl-data-table__cell--non-numeric" ] [ text tech.name ]
         , td [] [ text (toString tech.votes) ]
         , td []
             [editBtn tech]
@@ -74,4 +86,4 @@ editBtn tech =
             [ class "btn regular"
             , href path
             ]
-            [ i [ class "fa fa-pencil mr1" ] [], text "Edit" ]
+            [ i [ class "fa fa-pencil mr1" ] []]
